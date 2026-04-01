b7ad14ed800adc5d9a67ad61bbe3fa961e714a96
Vishant Bhardwaj <bhardwajvishant52@gmail.com>
2026-04-01 23:14:11 +0530

    feat(api): add search and filter functionality to GET /equipment

--- app/api/equipment/route.js
+++ app/api/equipment/route.js
@@ -2,11 +2,28 @@ import connectDB from "../../../lib/mongodb";
 import Equipment from "../../../models/Equipment";
 
 // GET → saare equipments fetch karega
-export async function GET() {
-  await connectDB();
-  const data = await Equipment.find();
-  return Response.json(data);
-}
+export async function GET(req) {
+    await connectDB();
+  
+    const { searchParams } = new URL(req.url);
+  
+    const search = searchParams.get("search");
+    const status = searchParams.get("status");
+  
+    let query = {};
+  
+    if (search) {
+      query.name = { $regex: search, $options: "i" };
+    }
+  
+    if (status) {
+      query.status = status;
+    }
+  
+    const data = await Equipment.find(query);
+  
+    return Response.json(data);
+  }
 
 // POST → naya equipment add karega
 export async function POST(req) {
