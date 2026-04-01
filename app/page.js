19c3c49426c40de43b5ca652bec74df6341a07b9
Vishant Bhardwaj <bhardwajvishant52@gmail.com>
2026-04-02 00:33:03 +0530

    feat(ui): add search and filter functionality with API integration

--- app/page.js
+++ app/page.js
@@ -1,37 +1,70 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import EquipmentCard from "@/components/EquipmentCard";
-async function getEquipments() {
-  const res = await fetch("http://localhost:3000/api/equipment", {
-    cache: "no-store",
-  });
 
-  return res.json();
-}
+export default function Home() {
+  const [equipments, setEquipments] = useState([]);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
 
-export default async function Home() {
-  const equipments = await getEquipments();
+  async function fetchData() {
+    let url = `/api/equipment?search=${search}&status=${status}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setEquipments(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [search, status]);
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-     <div className="flex justify-between items-center mb-6">
-  <h1 className="text-3xl font-bold text-green-700">
-    Green Grid
-  </h1>
+      
+      {/* Top bar */}
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold text-green-700">
+          Green Grid
+        </h1>
 
-  <a
-    href="/add"
-    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700"
-  >
-    + Add Equipment
-  </a>
-</div>
+        <a
+          href="/add"
+          className="bg-green-600 text-white px-4 py-2 rounded-lg"
+        >
+          + Add Equipment
+        </a>
+      </div>
 
+      {/* Search + Filter */}
+      <div className="flex gap-4 mb-6">
+        <input
+          type="text"
+          placeholder="Search equipment..."
+          className="border p-2 rounded w-full"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <select
+          className="border p-2 rounded"
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="available">Available</option>
+          <option value="in_use">In Use</option>
+        </select>
+      </div>
+
+      {/* List */}
       {equipments.length === 0 ? (
-        <p className="text-gray-600">No equipment found</p>
+        <p>No equipment found</p>
       ) : (
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {equipments.map((item) => (
-  <EquipmentCard key={item._id} item={item} />
-))}
+            <EquipmentCard key={item._id} item={item} />
+          ))}
         </div>
       )}
     </div>
